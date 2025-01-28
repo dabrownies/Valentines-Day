3f25be75a4b6ea53e40f7338a0507a09d71e230b
Adam Sadov <adam.sadov56@icloud.com>
2025-01-28 15:46:13 -0500

    added animations

--- app/page.js
+++ app/page.js
@@ -1,9 +1,40 @@
-import Image from "next/image";
+"use client"
+import { TypeAnimation } from "react-type-animation";
+import React, {useState} from "react";
 
 export default function Home() {
+  const [showImage, setShowImage] = useState(false)
+
   return (
-    <div>
-      
+    <div className="flex items-center justify-center h-screen text-center mb-10">
+      {!showImage ? (
+        <TypeAnimation
+          sequence={[
+            "Hi.", 2000,
+            "I have a question.", 2000,
+            "And there is only one right answer...", 2000,
+            "So.", 2000,
+            "When I ask it, think very carefully.", 2000,
+            "No pressure.", 2000,
+            () => setShowImage(true),
+          ]}
+          wrapper="span"
+          speed={50}
+          style={{ 
+            fontSize: "1.9em", 
+            display: "inline-block",
+            color: "black",
+            fontWeight: "bold"
+          }}
+          repeat={0}
+        />
+      ) : (
+        <img
+          src="/images/IMG_0999.GIF"
+          alt="Centered"
+          className="opacity-0 animate-fade-in w-1/2"
+        />
+      )}
     </div>
   );
 }
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,6 +7,15 @@ export default {
   ],
   theme: {
     extend: {
+      keyframes: {
+        "fade-in": {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+      },
+      animation: {
+        "fade-in": "fade-in 1.5s ease-in-out forwards",
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
